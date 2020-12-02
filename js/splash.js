1f0d76b537e389c4c765cf16174a366095234700
Benjamin Dryden <benjamin@adventureclub.io>
2020-12-02 20:10:57 +0100

    Splash screen with text mmovement

--- js/splash.js
+++ js/splash.js
@@ -1,9 +1,8 @@
 export default class SplashScreen {
     constructor(props) {
         this.splash = document.querySelector('.splash');
-        this.initialText = document.querySelector('.initial-text');
-        this.leftText = document.querySelector('.left-text');
-        this.rightText = document.querySelector('.right-text');
+        // this.initialText = document.querySelector('.initial-text');
+        // this.secondaryText = document.querySelector('.secondary-text');
         this.onLoad();
 
 
@@ -12,9 +11,9 @@ export default class SplashScreen {
     onLoad() {
         document.addEventListener('DOMContentLoaded', (e)=> {
             setTimeout(()=>{
-                // this.splash.classList.add('display-none');
+                this.splash.classList.add('display-none');
                 console.log('reached here');
-            }, 4000); //timeout should be other transition times summed.
+            }, 4500);
         })
     }
 
